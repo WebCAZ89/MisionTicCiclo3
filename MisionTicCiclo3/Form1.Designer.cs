af6bfbd9c79106bbf1e9fec141fd7f88d6961445
WebCAZ89 <webcaz89@gmail.com>
2021-10-03 13:30:49 -0500

    Agregar archivos de proyecto.

---
+++ MisionTicCiclo3/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace MisionTicCiclo3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grid = new System.Windows.Forms.DataGridView();
+            this.lblIdClientes = new System.Windows.Forms.Label();
+            this.lblNombreClientes = new System.Windows.Forms.Label();
+            this.lblDireccion = new System.Windows.Forms.Label();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.lblForm = new System.Windows.Forms.Label();
+            this.txtIdClientes = new System.Windows.Forms.TextBox();
+            this.txtNombreClientes = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grid
+            // 
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.grid.Location = new System.Drawing.Point(31, 78);
+            this.grid.Name = "grid";
+            this.grid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grid.Size = new System.Drawing.Size(498, 288);
+            this.grid.TabIndex = 0;
+            // 
+            // lblIdClientes
+            // 
+            this.lblIdClientes.AutoSize = true;
+            this.lblIdClientes.Location = new System.Drawing.Point(535, 82);
+            this.lblIdClientes.Name = "lblIdClientes";
+            this.lblIdClientes.Size = new System.Drawing.Size(56, 13);
+            this.lblIdClientes.TabIndex = 1;
+            this.lblIdClientes.Text = "Id Clientes";
+            // 
+            // lblNombreClientes
+            // 
+            this.lblNombreClientes.AutoSize = true;
+            this.lblNombreClientes.Location = new System.Drawing.Point(545, 133);
+            this.lblNombreClientes.Name = "lblNombreClientes";
+            this.lblNombreClientes.Size = new System.Drawing.Size(44, 13);
+            this.lblNombreClientes.TabIndex = 2;
+            this.lblNombreClientes.Text = "Nombre";
+            // 
+            // lblDireccion
+            // 
+            this.lblDireccion.AutoSize = true;
+            this.lblDireccion.Location = new System.Drawing.Point(539, 180);
+            this.lblDireccion.Name = "lblDireccion";
+            this.lblDireccion.Size = new System.Drawing.Size(52, 13);
+            this.lblDireccion.TabIndex = 3;
+            this.lblDireccion.Text = "Dirección";
+            // 
+            // lblTelefono
+            // 
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.Location = new System.Drawing.Point(540, 229);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(49, 13);
+            this.lblTelefono.TabIndex = 4;
+            this.lblTelefono.Text = "Teléfono";
+            // 
+            // lblForm
+            // 
+            this.lblForm.AutoSize = true;
+            this.lblForm.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblForm.Location = new System.Drawing.Point(196, 9);
+            this.lblForm.Name = "lblForm";
+            this.lblForm.Size = new System.Drawing.Size(425, 37);
+            this.lblForm.TabIndex = 5;
+            this.lblForm.Text = "Formulario CLientes MITIC";
+            // 
+            // txtIdClientes
+            // 
+            this.txtIdClientes.Location = new System.Drawing.Point(630, 79);
+            this.txtIdClientes.Name = "txtIdClientes";
+            this.txtIdClientes.Size = new System.Drawing.Size(129, 20);
+            this.txtIdClientes.TabIndex = 6;
+            // 
+            // txtNombreClientes
+            // 
+            this.txtNombreClientes.Location = new System.Drawing.Point(630, 132);
+            this.txtNombreClientes.Name = "txtNombreClientes";
+            this.txtNombreClientes.Size = new System.Drawing.Size(129, 20);
+            this.txtNombreClientes.TabIndex = 7;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(630, 180);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(129, 20);
+            this.txtDireccion.TabIndex = 8;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(630, 229);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(128, 20);
+            this.txtTelefono.TabIndex = 9;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(543, 282);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(215, 28);
+            this.btnAgregar.TabIndex = 10;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(543, 343);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(107, 24);
+            this.btnModificar.TabIndex = 11;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(680, 342);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(77, 24);
+            this.btnBorrar.TabIndex = 12;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.txtNombreClientes);
+            this.Controls.Add(this.txtIdClientes);
+            this.Controls.Add(this.lblForm);
+            this.Controls.Add(this.lblTelefono);
+            this.Controls.Add(this.lblDireccion);
+            this.Controls.Add(this.lblNombreClientes);
+            this.Controls.Add(this.lblIdClientes);
+            this.Controls.Add(this.grid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grid;
+        private System.Windows.Forms.Label lblIdClientes;
+        private System.Windows.Forms.Label lblNombreClientes;
+        private System.Windows.Forms.Label lblDireccion;
+        private System.Windows.Forms.Label lblTelefono;
+        private System.Windows.Forms.Label lblForm;
+        private System.Windows.Forms.TextBox txtIdClientes;
+        private System.Windows.Forms.TextBox txtNombreClientes;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnBorrar;
+    }
+}
+
---
+++ MisionTicCiclo3/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MisionTicCiclo3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cargar();
+        }
+
+        private void cargar()
+        {
+            BDMiticEntitiesConection contexto = new BDMiticEntitiesConection();
+            grid.DataSource = contexto.ClientesMitic.ToList();
+        }
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(this.txtIdClientes.Text);
+            string nombre = this.txtNombreClientes.Text;
+            string direccion = this.txtDireccion.Text;
+            string telefono = this.txtTelefono.Text;
+
+            using(BDMiticEntitiesConection contexto = new BDMiticEntitiesConection ())
+            {
+                ClientesMitic c = new ClientesMitic
+                {
+                    idClientes = id,
+                    nombreClientes = nombre,
+                    direccion = direccion,
+                    telefono = telefono
+                };
+               
+                contexto.ClientesMitic.Add(c);
+                contexto.SaveChanges();
+                cargar();
+            }
+
+
+        }
+    }
+}
+ 
